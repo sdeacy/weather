2cfed4d0e4d9c5f08b8b6758283ea41c23bc6108
shay deacy <shay@shays-mbp.lan>
2014-12-16 13:58:10 +0000

    first commit

---
+++ SDFiveDayForecast/DayForecastTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DayForecastTableViewCell.h
+//  SDFiveDayForecast
+//
+//  Created by shay deacy on 16/12/2014.
+//  Copyright (c) 2014 shay deacy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DayForecastTableViewCell : UITableViewCell
+
+@end
---
+++ SDFiveDayForecast/DayForecastTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  DayForecastTableViewCell.m
+//  SDFiveDayForecast
+//
+//  Created by shay deacy on 16/12/2014.
+//  Copyright (c) 2014 shay deacy. All rights reserved.
+//
+
+#import "DayForecastTableViewCell.h"
+
+@implementation DayForecastTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
